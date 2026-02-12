b7b35fc49c57d6f69cfed4e7294fc55a8100fd15
Giuseppe Costantino <giuseppe.costantino95@gmail.com>
2026-02-12 13:50:05 +0100

    Update __init__.py
    added catalogues support

--- src/trajmod/events/__init__.py
+++ src/trajmod/events/__init__.py
@@ -1,15 +1,34 @@
 """Event catalog and selection module."""
-
-from trajmod.events.events import EventCatalog
+from trajmod.events.catalogs import CatalogAdapter, USGSAdapter, CatalogFetchError
+from trajmod.events.catalogs.catalog_fetcher import CatalogFetcher
+from trajmod.events.catalogs.validation import validate_earthquake_catalog, validate_sse_catalog, \
+    print_validation_summary
 from trajmod.events.event_selection import (
     EventSelector,
     KneeEventSelector,
-    LassoEventSelector,
+    LassoEventSelector, MarginalScreeningSelector, ThresholdEventSelector,
 )
+from trajmod.events.events import EventCatalog
 
 __all__ = [
+    # Main classes
+    "CatalogFetcher",
     "EventCatalog",
+
+    # Event selection
     "EventSelector",
     "KneeEventSelector",
     "LassoEventSelector",
+    "MarginalScreeningSelector",
+    "ThresholdEventSelector",
+
+    # Advanced (catalog adapters)
+    "CatalogAdapter",
+    "USGSAdapter",
+    "CatalogFetchError",
+
+    # catalog validation
+    "validate_earthquake_catalog",
+    "validate_sse_catalog",
+    "print_validation_summary"
 ]
